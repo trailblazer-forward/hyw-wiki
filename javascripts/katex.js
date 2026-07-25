42891515f0f04e48b540c4ee6e0486940ed83058
trailblazer-forward <trailblazer-forward@users.noreply.github.com>
2026-07-25 07:55:51 +0000

    deploy: 0f260fbcec921f785ba48147ba1f79ea964986ba

--- javascripts/katex.js
+++ javascripts/katex.js
@@ -1,10 +1,19 @@
-document$.subscribe(({ body }) => { 
+document$.subscribe(({ body }) => {
   renderMathInElement(body, {
     delimiters: [
-      { left: "$$",  right: "$$",  display: true },
-      { left: "$",   right: "$",   display: false },
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
       { left: "\\(", right: "\\)", display: false },
-      { left: "\\[", right: "\\]", display: true }
+      { left: "\\[", right: "\\]", display: true },
     ],
-  })
-})
+    macros: {
+      "\\electronTrajectory":
+        "\\overset{\\mathrm{#1}}{\\begin{array}{#2} \\hline #3 \\\\ \\hline \\end{array}}",
+      "\\etS": "\\electronTrajectory{#1s}{|c|}{#2}",
+      "\\etP": "\\electronTrajectory{#1p}{|c|c|c|}{#2}",
+      "\\etD": "\\electronTrajectory{#1d}{|c|c|c|c|c|}{#2}",
+      "\\etF": "\\electronTrajectory{#1d}{|c|c|c|c|c|c|c|}{#2}",
+      "\\emptyT": "\\hspace{0.5em}",
+    },
+  });
+});
